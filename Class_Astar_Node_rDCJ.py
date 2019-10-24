08fc824f7f661a3b6734336e2f745431a65ba69a
HFouch <40229797+HFouch@users.noreply.github.com>
2019-10-24 10:50:28 +0200

    Update Class_Astar_Node_rDCJ.py

--- Class_Astar_Node_rDCJ.py
+++ Class_Astar_Node_rDCJ.py
@@ -295,7 +295,8 @@ class Node:
 
 
             # else if the element is a telomere
-            elif type(element) is str:
+            #elif type(element) is str:
+            else:
                 u = 0
                 p = element
 
